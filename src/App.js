45e970a4a8d23c398bb5c91a1d8653e05ec1cfc4
abdulrahman <abdulrahman102034@gmail.com>
2024-11-25 18:14:31 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Landing_page from './Components/Landing_Page/LandingPage';
 import Login from './Components/Login/Login';
 import Sign_Up from "./Components/Sign_Up/Sign_Up";
+import InstantConsultation from "./Components/InstantConsultationBooking/InstantConsultation";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/" element={<Landing_page/>}/>
                 <Route path="/login" element= {<Login/>} />
                 <Route path="/signup" element= {<Sign_Up/>} />
+                <Route path="/instant-consultation" element={<InstantConsultation />} />
             </Routes>
         </BrowserRouter>
     </div>
